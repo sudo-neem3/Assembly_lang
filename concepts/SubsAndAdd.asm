5e1b7181060a34ef18e479d794752c7b7185e239
sudo-neem3 <neemkhalid0@gmail.com>
2026-02-15 15:32:27 +0500

    SubsAndAdd.asm

---
+++ concepts/SubsAndAdd.asm
@@ -0,0 +1,13 @@
+; This code is for Addtion 2 and subtract 2 numbers.;
+
+org 100h
+
+ mov ax,10
+ ; 1st number 10;
+ mov bx,6
+ add ax,bx
+ mov bx,34
+ sub bx,ax
+; sub is used for subtraction.;
+ret
+
